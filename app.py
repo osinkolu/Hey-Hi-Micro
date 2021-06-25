54c80bd91ef84097aa837dda4c35df269980d0b3
Olufemi Victor <60588823+osinkolu@users.noreply.github.com>
2021-06-25 12:09:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ async def predict(request:Request, file:UploadFile = File(...)):
             face_finder = "I couldn't find your face in the image, but still"
                 
             print(confidence)
-        image_path = "tmp/temporary.jpg"
+        image_path = "static/tmp/temporary.jpg"
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         (h, w) = image.shape[:2]
         box = detections[0,0,0,3:7] * np.array([w,h,w,h])
